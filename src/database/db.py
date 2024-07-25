9564da69540e1f5072fa7b4e1881be29f076c338
Yoander Robles <yoander.robles@crv-soft.com>
2024-07-24 23:57:14 -0400

    creacion de usuario listo

--- src/database/db.py
+++ src/database/db.py
@@ -16,8 +16,6 @@ SessionLocal = sessionmaker(class_=AsyncSession ,autocommit=False, autoflush=Fal
 
 Base: DeclarativeMeta = declarative_base()
 
-
-
 async def create_db_and_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all) # type: ignore
